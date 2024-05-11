a4440c0cf8167839ae940e5d0c994165c9e7b1ad
mdanash97 <m.elamirdanash@gmail.com>
2024-05-11 17:38:23 +0300

    groups and courses ui

---
+++ app/src/main/java/com/omar/sams/Student/CoursesInfoActivity.java
@@ -0,0 +1,16 @@
+package com.omar.sams.Student;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.omar.sams.R;
+
+public class CoursesInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_courses_info);
+    }
+}
