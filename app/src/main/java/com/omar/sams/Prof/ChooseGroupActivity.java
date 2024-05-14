b2a40e4c83cbe0b944d9451d984990ca032aa161
mdanash97 <132082850+mdanash97@users.noreply.github.com>
2024-05-14 19:27:20 +0300

    ui modifications

---
+++ app/src/main/java/com/omar/sams/Prof/ChooseGroupActivity.java
@@ -0,0 +1,16 @@
+package com.omar.sams.Prof;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.omar.sams.R;
+
+public class ChooseGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_group);
+    }
+}
