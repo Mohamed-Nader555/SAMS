9c1bbdcd155cd668ad5c775b5b9f362efc8c840c
Mohamed-Nader555 <mohamed.m.nader555@gmail.com>
2024-05-14 23:22:14 +0300

    Test StudentDashboardActivity

--- app/src/main/java/com/omar/sams/Student/StudentDashboardActivity.java
+++ app/src/main/java/com/omar/sams/Student/StudentDashboardActivity.java
@@ -1,6 +1,7 @@
 package com.omar.sams.Student;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +9,28 @@ import com.omar.sams.R;
 
 public class StudentDashboardActivity extends AppCompatActivity {
 
+
+    ImageView eContentBtn, clubsBtn, contactBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_dashboard);
+
+
+        initView();
+
     }
+
+    private void initView() {
+
+        eContentBtn = findViewById(R.id.studentEcontentBtn);
+        clubsBtn = findViewById(R.id.studentClubsBtn);
+        contactBtn = findViewById(R.id.studentContactBtn);
+
+
+    }
+
+
+
 }
